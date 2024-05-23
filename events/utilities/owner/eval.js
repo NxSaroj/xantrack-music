2fb2e2e0f27852ed98ff9b543ad088fc955a663f
Syst69 <148795898+Syst69@users.noreply.github.com>
2024-05-23 19:14:17 +0530

    Fixed eval command

--- events/utilities/owner/eval.js
+++ events/utilities/owner/eval.js
@@ -1,5 +1,5 @@
 const { Events } = require("discord.js");
-const { management } = require('../../../config.json')
+const { management } = require("../../../config.json");
 module.exports = {
   name: Events.MessageCreate,
   once: false,
@@ -9,13 +9,19 @@ module.exports = {
    * @param {import('discord.js').Client} client
    */
   execute: async (message, client) => {
-    if (message.author.id !== management[0].discordId) return message.reply(`<:xn_pink_wrong:1215934948075110441> **Cutie This command is owner only**`)
-    const code = message.content.slice(".run".length).trim();
-    const reply = await message.channel.send(`Evaluating the code...`);
-    const output = eval(code);
-    if (eval instanceof Promise) await output;
-    await reply.edit(`\`\`\`js\n${output}\n\`\`\``).catch((err) => {
-      return reply.edit(`\`\`\`js\n${err}\n\`\`\``);
-    });
+    if (message.content.startsWith(".eval")) {
+      if (message.author.id !== management[0].discordId) {
+        return await message.reply(
+          `<:xn_pink_wrong:1215934948075110441> **Cutie This command is owner only**`
+        );
+      }
+      const code = message.content.slice(".eval".length).trim();
+      const reply = await message.channel.send(`Evaluating the code...`);
+      const output = eval(code);
+      if (eval instanceof Promise) await output;
+      await reply.edit(`\`\`\`js\n${output}\n\`\`\``).catch((err) => {
+        return reply.edit(`\`\`\`js\n${err}\n\`\`\``);
+      });
+    }
   },
 };
